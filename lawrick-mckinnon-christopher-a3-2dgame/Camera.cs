71e3a62de0030ff82baf03391bf12b37bf838ddd
ChristopherMcKinnon <christopher.lawrick-mckinnon@mohawkcollege.ca>
2026-03-11 16:54:47 -0400

    Implement Camera Test

---
+++ lawrick-mckinnon-christopher-a3-2dgame/Camera.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace MohawkGame2D
+{
+    internal class Camera
+    {
+        Matrix4x4 projectionMatrix;
+        Matrix4x4 viewMatrix;
+        Matrix4x4 viewProjectionMatrix;
+
+        Vector2 position;
+        float rotation = 0.0f;
+        float scale = 1.0f;
+
+        public Camera(float left, float right, float bottom, float top)
+        {
+
+        }
+        
+        // Get and Set methods
+        public void SetPosition(Vector2 position){this.position = position;}
+        public Vector2 GetPosition(){ return position; }
+        public void SetRotation(float rotation){this.rotation = rotation;}
+        public float GetRotation(){ return rotation;  }
+        public void SetScale(float scale){this.scale = scale;}
+        public float GetScale() {  return scale; }
+
+
+        // Convert world to screen
+        public Vector2 WorldToScreenPos(Vector2 objectPosition)  
+        {
+            Vector2 screenPosition;
+            screenPosition.X = objectPosition.X - this.GetPosition().X + (this.GetScale() - 1.0f)*(objectPosition.X - this.GetPosition().X - Window.Width/2);
+            screenPosition.Y = objectPosition.Y - this.GetPosition().Y + (this.GetScale() - 1.0f)*(objectPosition.Y - this.GetPosition().Y - Window.Height/2);
+
+
+            return screenPosition;
+        }
+    }
+}
--- lawrick-mckinnon-christopher-a3-2dgame/Game.cs
+++ lawrick-mckinnon-christopher-a3-2dgame/Game.cs
@@ -1,32 +1,85 @@
-﻿// Include the namespaces (code libraries) you need below.
-using System;
+﻿using System;
 using System.Numerics;
 
-// The namespace your code is in.
+
 namespace MohawkGame2D
 {
-    /// <summary>
-    ///     Your game code goes inside this class!
-    /// </summary>
+    
     public class Game
     {
-        // Place your variables here:
-        
 
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
+        Camera cam;
+        Vector2 squarePos = new(400, 300);
+        Vector2 squareSize = new(25, 25);
+
         public void Setup()
         {
-            
+            Window.SetTitle("2D Game");
+            Window.SetSize(800, 600);
+            Window.TargetFPS = 60;
+
+            BeginScene();
         }
 
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
+        public void BeginScene()
+        {
+            cam = new Camera(10, 10, 10, 10);
+            cam.SetPosition(new Vector2(20,20));
+            cam.SetScale(1.0f);
+        }
+        public void Render(int spacing)
+        {
+            Draw.LineSize = 1;
+            Draw.LineColor = new Color(255, 0, 0);
+            for (int i = 0; i < (Window.Width + Window.Width); i += spacing)
+            {
+                Draw.Line(i, 0, i, Window.Height);
+            }
+            for (int i = 0; i < (Window.Height + Window.Height); i += spacing)
+            {
+                Draw.Line(0, i, Window.Width, i);
+            }
+
+            Draw.LineColor = Color.Blue;
+            Draw.Circle(cam.GetPosition(), 5f);
+
+            Draw.Rectangle(cam.WorldToScreenPos(squarePos), squareSize*cam.GetScale());
+
+
+        }
+        public void ControlsUpdate()
+        {
+            if (Input.IsKeyboardKeyDown(KeyboardInput.S))
+            {
+                cam.SetPosition(cam.GetPosition() + new Vector2(0, 1));
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.W))
+            {
+                cam.SetPosition(cam.GetPosition() + new Vector2(0, -1));
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.A))
+            {
+                cam.SetPosition(cam.GetPosition() + new Vector2(-1, 0));
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.D))
+            {
+                cam.SetPosition(cam.GetPosition() + new Vector2(1, 0));
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.E))
+            {
+                cam.SetScale(cam.GetScale()+0.01f);
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Q))
+            {
+                cam.SetScale(cam.GetScale()-0.01f);
+            }
+        }
         public void Update()
         {
+            Window.ClearBackground(Color.Black);
             
+            Render(40);
+            ControlsUpdate();
         }
     }
 
